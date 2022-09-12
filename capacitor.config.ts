3310137c82c445ba34cec49c12ed9d4a8e45348a
Alex Lopes <lopale02@csgicorp.com>
2022-09-12 15:53:07 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Leilao',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
